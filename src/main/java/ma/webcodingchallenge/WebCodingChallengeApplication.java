fb1a9862f492eeefe006582f763f9cc7274fb75c
HAMZA AIT OUMGHAR <hamwac5@yahoo.fr>
2018-11-15 21:15:55 +0000

    first commit

---
+++ src/main/java/ma/webcodingchallenge/WebCodingChallengeApplication.java
@@ -0,0 +1,12 @@
+package ma.webcodingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCodingChallengeApplication.class, args);
+	}
+}
---
+++ src/test/java/ma/webcodingchallenge/WebCodingChallengeApplicationTests.java
@@ -0,0 +1,16 @@
+package ma.webcodingchallenge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebCodingChallengeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
